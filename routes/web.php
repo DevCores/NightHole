c3412ccc1e391b9950ac26df51859c7c25ff07b9
DevCoress <emudarisov@outlook.com>
2021-03-14 19:00:00 +1000

    Фикс переадресации

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/setlocale/{locale}', function ($locale) {
     
     	Session::put('locale', $locale);                    # И устанавливаем его в сессии под именем locale
 
-    return redirect()->back();                              # Редиректим его <s>взад</s> на ту же страницу
+    return redirect('welcome');                              # Редиректим его <s>взад</s> на ту же страницу
 
 });
 
